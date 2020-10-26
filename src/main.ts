d60b6bc7964e4ba82a76786d49cb8c181fb8df16
Brett Onions <onionsllb@outlook.com>
2020-10-26 08:38:12 +0200

    Adding API Documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as dotenv from "dotenv";
+dotenv.config();
 
 async function bootstrap() {
+  const version = process.env.TERMINOLOGY_SERVICE_API_VERSION;
+
   const app = await NestFactory.create(AppModule);
   
   const options = new DocumentBuilder()
@@ -12,6 +16,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
+  app.setGlobalPrefix(`api/${version}`);
 
   await app.listen(3001);
 }
